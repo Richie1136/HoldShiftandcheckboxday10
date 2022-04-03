17c2f80e8745e233f06f027f09286839da8b3ca0
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-03 10:52:28 -0400

    Finished JS30 day10 in which i looped over the checkboxes and if the set the checked property on the checkbox to true if the checkbox was between this or the last checked variable

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]')
 
-const handleCheck = (event) => {
-  console.log(event)
+
+let lastChecked;
+
+function handleCheck(event) {
+  // Check if the shift key is down and check that it is being checked
+
+  let inBetween = false
+
+  if (event.shiftKey && this.checked) {
+    // looping over every single checkbox
+    checkboxes.forEach(checkbox => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween
+      }
+      if (inBetween) {
+        checkbox.checked = true
+      }
+    });
+  }
+  lastChecked = this
 }
 
 
